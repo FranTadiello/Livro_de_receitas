544c283dcc22f7c87121ee0024fbde7432663365
Franciele Tadiello <franciele.p.t@gmail.com>
2023-08-04 14:29:26 -0300

    Criando Rotas e Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+/* react router dom */
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+/* pages */
+import { Inicio, Cadastro, Error, Receitas } from "./pages/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter /*Roteador*/> 
+      <Routes>
+        <Route path = "/" element = {<Inicio />} />
+        <Route path = "/cadastrar-receita" element = {<Cadastro />} />
+        <Route path = "/lista-de-receitas" element = {<Receitas />} />
+        <Route path  = "*" element = {<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Inicio from "../pages/InicioPage/InicioPage";
+import Error from "../pages/ErrorPage/ErrorPage";
+import Cadastro from "../pages/CadastroPage/CadastroPage";
+import Receitas from "../pages/ReceitasPage/ReceitasPage";
+
+export { Inicio, Error, Cadastro, Receitas };
